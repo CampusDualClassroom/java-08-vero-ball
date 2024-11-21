16cf4a255e3beaf6d78a56310e336679024c452b
vero-ball <v.rivadulla.deschamps@gmail.com>
2024-11-21 14:33:47 +0100

    ej8 err

--- src/main/java/com/campusdual/classroom/Exercise08.java
+++ src/main/java/com/campusdual/classroom/Exercise08.java
@@ -3,7 +3,8 @@ package com.campusdual.classroom;
 public class Exercise08 {
 
     public static void main(String[] args) {
-
+        int num = 9;
+        whileLoop(num);
     }
 
     //TODO ↓
@@ -11,7 +12,13 @@ public class Exercise08 {
     // X < Y. El próximo ciclo valdrá: X+1
     // X+1 < Y. El próximo ciclo valdrá: X+2
     // etc.
-    public static void whileLoop(int num) {
 
+    public static void whileLoop(int num) {
+        int i = 0;
+        while(i < num){
+            System.out.println(i + " < "+ num + ". El próximo ciclo valdrá: " + (i+1));
+            System.out.println((i+1) + " < "+ num + ". El próximo ciclo valdrá: " + (i+2));
+            i++;
+        }
     }
 }
